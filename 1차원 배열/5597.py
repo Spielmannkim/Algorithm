fabb5b66a54d1155d01d966d20d1b87696980164
spielmannkim <shinjongk@gmail.com>
2022-12-01 17:58:43 +0900

    과제 안내신분..?

--- 1차원 배열/5597.py
+++ 1차원 배열/5597.py
@@ -1,8 +1,8 @@
-n = [i for i in range(1, 31)] # 
+n = [i for i in range(1, 31)] # 1-30까지 정수 리스트
 
-for _ in range(28):
-    all = int(input())
-    n.remove(all)
+for _ in range(28): # 28번 반복
+    all = int(input()) # 참석한 학생번호 하나씩 입력
+    n.remove(all) # 전체 학생n 중 참석한 학생all을 빼서 최종 결석자 리스트 생성
     
-print(min(n))
-print(max(n))
+print(min(n)) # 결석자중 최솟값을 통해 번호앞자리 결석자 출력
+print(max(n)) # 결석자중 최댓값을 통해 번호뒷자리 결석자 출력
